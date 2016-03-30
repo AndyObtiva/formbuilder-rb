fa0dc0db1854465d612f8c64d8976518a5c41f58
AndyObtiva <andy.am@gmail.com>
2016-03-30 15:08:19 -0400

    Refactoring and cleaning code for file field render_input

--- app/models/formbuilder/response_field_file.rb
+++ app/models/formbuilder/response_field_file.rb
@@ -19,12 +19,12 @@ module Formbuilder
     end
 
     def render_input(value, opts = {})
-      entry_attachment = Formbuilder::EntryAttachment.find_by_id(value)
+      entry_attachment = get_attachments(value).first
       upload = entry_attachment.try(:upload)
       remote_upload_url = entry_attachment.try(:remote_upload_url)
       """
-        <span class='existing-filename'>#{get_attachments(value).first.try(:upload).try(:raw_filename)}</span>
-        <input type='file' name='response_fields[#{self[:id]}][]' id='response_fields_#{self[:id]}' value='#{upload}' />
+        <span class='existing-filename' style='cursor: default;'>#{upload}</span>
+        <input type='file' name='response_fields[#{self[:id]}][]' id='response_fields_#{self[:id]}'' />
         <input type='hidden' name='response_fields[#{self[:id]}][]' id='response_fields_#{self[:id]}_buffer' class='file-buffer' value='#{remote_upload_url}' />
       """
     end
